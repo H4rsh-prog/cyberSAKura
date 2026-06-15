28363d85c017f9b785045d60855862ce7d906af1
Harsh <h4rshshrivastava@gmail.com>
2026-06-15 13:38:10 +0530

    working on ms-user for basic auth

---
+++ user-service/src/main/java/com/cyberSAKura/service/user/dto/UserRepository.java
@@ -0,0 +1,9 @@
+package com.cyberSAKura.service.user.dto;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, String>{
+
+}
---
+++ user-service/src/main/java/com/cyberSAKura/service/user/exception/UsernameNotFoundException.java
@@ -0,0 +1,9 @@
+package com.cyberSAKura.service.user.exception;
+
+public class UsernameNotFoundException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6902450692785024564L;
+}
