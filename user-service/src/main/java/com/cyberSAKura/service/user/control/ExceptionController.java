6503d766fa437b59a03433caedc3aae7a002a508
Harsh <h4rshshrivastava@gmail.com>
2026-06-15 18:45:48 +0530

    more stuff for user service

--- user-service/src/main/java/com/cyberSAKura/service/user/control/ExceptionController.java
+++ user-service/src/main/java/com/cyberSAKura/service/user/control/ExceptionController.java
@@ -1,21 +1,35 @@
 package com.cyberSAKura.service.user.control;
 
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
 import org.apache.hc.core5.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.cyberSAKura.service.user.exception.UsernameNotFoundException;
+import com.cyberSAKura.service.user.exception.model.DescribedRuntimeException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ControllerAdvice
 public class ExceptionController {
 	private ObjectMapper mapper = new ObjectMapper();
+	private Logger log = Logger.getLogger(this.getClass().getName());
+	
+	public void logErr(DescribedRuntimeException ex) {
+		log.severe("RUNTIME EXCEPTION : "+ex.getClass().getName()+" : "+ex.getDescription());
+	}
 	
 	@ExceptionHandler(exception = UsernameNotFoundException.class)
-	ResponseEntity<?> usernameNotFoundException() {
-		System.err.println("RUNTIME EXCEPTION :: USERNAME NOT FOUND");
-		record response(String status, String message) {}
-		return ResponseEntity.status(HttpStatus.SC_BAD_REQUEST).body(new response("failed", "No database entries found associated with the queried username"));
+	ResponseEntity<?> usernameNotFoundException(UsernameNotFoundException ex) {
+		logErr(ex);
+		record StackTraceRecord(String className, String fileName, long lineNumber, String methodName) {}
+		ArrayList<StackTraceRecord> stackTrace = new ArrayList<>();
+		for(StackTraceElement element : ex.getStackTrace()) {
+			stackTrace.add(new StackTraceRecord(element.getClassName(), element.getFileName(), element.getLineNumber(), element.getMethodName()));
+		}
+		record response(String status, String message, ArrayList<StackTraceRecord> stackTrace) {}
+		return ResponseEntity.status(HttpStatus.SC_BAD_REQUEST).body(new response("failed", "No database entries found associated with the queried username", stackTrace));
 	}
 }
