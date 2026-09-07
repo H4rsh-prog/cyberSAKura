7e4e92b6c759c6bb7883b6b083ec2e3b7f8862cd
Harsh <h4rshshrivastava@gmail.com>
2026-09-07 17:04:52 +0530

    implemented dac appraoch of dictionary creation, also removed verbosity logs for performance optimisation

--- MODULES/LZ77/src/main/java/com/tools/module/service/DictionaryService.java
+++ MODULES/LZ77/src/main/java/com/tools/module/service/DictionaryService.java
@@ -2,6 +2,7 @@ package com.tools.module.service;
 
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -16,19 +17,18 @@ import com.tools.module.model.ByteArrayWrapper;
 
 @Service
 public class DictionaryService {
-	private int maxBytesUsed = 1;
-	private boolean verbose = false;
-	private int dictionaryLimit = 254;
-	private int additionalMarkerBytes = 1;
+	private static int maxBytesUsed = 1;
+	private static int dictionaryLimit = 254;
+	static final int additionalMarkerBytes = 1;
 	
-	public ArrayList<ByteArrayWrapper> createDictionary(byte[] byteArr) {
+	//LINEAR APPROACH
+	public ArrayList<ByteArrayWrapper> createDictionaryLinear(byte[] byteArr) {
 		HashMap<ByteArrayWrapper, Integer> frequencyTable = new HashMap<>();
 		HashMap<ByteArrayWrapper, Integer> temp_frequencyTable = new HashMap<>();
 		HashSet<Byte> potentialBlockStart = new HashSet<Byte>();
 		int byteArrSize = byteArr.length;
 		int i;
 		for(i=0;i<100000 && i<byteArrSize;i++) {
-			if(this.verbose) System.out.print("FINDING REPETITION PROGRESS : ["+i+"/"+byteArrSize+"]\r");
 			if(potentialBlockStart.contains(byteArr[i])) {
 				ByteArrayWrapper dataBlock = findByteBlock(byteArr, temp_frequencyTable, i);
 				temp_frequencyTable.put(dataBlock, temp_frequencyTable.getOrDefault(dataBlock, 0)+1);
@@ -37,15 +37,14 @@ public class DictionaryService {
 				potentialBlockStart.add(byteArr[i]);
 			}
 		}
-		if(this.verbose) System.out.print("COMMITING FIRST REPETITION BATCH... \r");
 		for(Entry<ByteArrayWrapper, Integer> e: temp_frequencyTable.entrySet()) {
 			frequencyTable.put(e.getKey(), e.getValue());
 		}
 		temp_frequencyTable.clear();
+		
 		while(i<byteArrSize) {
 			int k;
 			for(k=i;k<i+100000 && k<byteArrSize;k++) {
-				if(this.verbose) System.out.print("FINDING REPETITION PROGRESS : ["+k+"/"+byteArrSize+"]\r");
 				if(potentialBlockStart.contains(byteArr[k])) {
 					ByteArrayWrapper dataBlock = findByteBlock(byteArr, temp_frequencyTable, k);
 					temp_frequencyTable.put(dataBlock, Math.max(frequencyTable.getOrDefault(dataBlock, 0), temp_frequencyTable.getOrDefault(dataBlock, 0))+1);
@@ -54,52 +53,15 @@ public class DictionaryService {
 					potentialBlockStart.add(byteArr[k]);
 				}
 			}
-			if(this.verbose) System.out.print("COMMITING REPETITION BATCH NO. ["+(int)k%100000+"] \r");
 			for(Entry<ByteArrayWrapper, Integer> e: temp_frequencyTable.entrySet()) {
 				frequencyTable.put(e.getKey(), frequencyTable.getOrDefault(e.getKey(), 0)+e.getValue());
 			}
 			temp_frequencyTable.clear();
 			i=k;
 		}
-		int preLength = frequencyTable.size();
-		for(ByteArrayWrapper invalidKeys : frequencyTable.keySet().stream().filter(new Predicate<ByteArrayWrapper>() {
-			@Override
-			public boolean test(ByteArrayWrapper t) {
-				return frequencyTable.get(t)==1;
-			}
-		}).toList()) {
-			frequencyTable.remove(invalidKeys);
-		}
-		int postLength = frequencyTable.size();
-		if(verbose) System.out.println("TABLE REDUCED BY ["+(preLength-postLength)+"] ENTRIES AFTER FILTERING NON REPEATING BLOCKS NOW WITH REMAINING ENTRIES : "+frequencyTable.size());
-		preLength = postLength;
-		if(preLength>255) {
-			if(preLength>Math.pow(2, 16)) {
-				if(preLength>Math.pow(2, 24)) {
-					this.maxBytesUsed = 4;
-				} else {
-					this.maxBytesUsed = 3;
-				}
-			} else {
-				this.maxBytesUsed = 2;
-			}
-		} else {
-			this.maxBytesUsed = 1;
-		}
-		this.dictionaryLimit = (int) Math.pow(255, this.maxBytesUsed);
-		if(this.verbose) System.out.println("SETTING DELIMITER TO ["+this.maxBytesUsed+":"+this.dictionaryLimit+"] BYTES WITH THE FREQUENCY TABLE ENTRIES EXCEEDING ["+preLength+"]");
-		for(ByteArrayWrapper invalidKeys : frequencyTable.keySet().stream().filter(new Predicate<ByteArrayWrapper>() {
-			@Override
-			public boolean test(ByteArrayWrapper t) {
-				return t.getData().length<=(maxBytesUsed+additionalMarkerBytes);
-			}
-		}).toList()) {
-			frequencyTable.remove(invalidKeys);
-		}
-		postLength = frequencyTable.size();
-		if(this.verbose) System.out.println("TABLE REDUCED BY ["+(preLength-postLength)+"] ENTRIES AFTER FILTERING BLOCKS LARGER THAN DICTIONARY LIMIT NOW REMAINING ENTRIES : "+frequencyTable.size());
-		return generateSortedBytesFromFrequency(frequencyTable);
+		return sanitizeFrequencies(frequencyTable);
 	}
+	
 	private ByteArrayWrapper findByteBlock(byte[] byteArr, HashMap<ByteArrayWrapper, Integer> frequencyTable, int startIndx) {
 		Set<ByteArrayWrapper> repeatingBlocks = frequencyTable.keySet();
 		byte[] start_byteArr = new byte[1];
@@ -121,17 +83,115 @@ public class DictionaryService {
 		}
 		return dataBlock;
 	}
-	private ArrayList<ByteArrayWrapper> generateSortedBytesFromFrequency(HashMap<ByteArrayWrapper, Integer> frequencyTable) {
+	//LINEAR APPROACH
+
+	//DAC APPROACH
+	public ArrayList<ByteArrayWrapper> createDictionaryDAC(byte[] byteArr) {
+		HashMap<ByteArrayWrapper, Integer> frequencyTable = new HashMap<>();
+		divideAndFindRepetition(byteArr, frequencyTable, 0);
+		return sanitizeFrequencies(frequencyTable);
+	}
+	public void divideAndFindRepetition(byte[] byteArr, HashMap<ByteArrayWrapper, Integer> currentFrequency, int depth) {
+		if(byteArr.length<=1) return;
+		ByteArrayWrapper wrapper = new ByteArrayWrapper(byteArr);
+		int len = byteArr.length;
+		int mid = len/2;
+		currentFrequency.put(wrapper, currentFrequency.getOrDefault(wrapper, 0)+1);
+		divideAndFindRepetition(Arrays.copyOfRange(byteArr, 0, mid), currentFrequency, depth+1);
+		divideAndFindRepetition(Arrays.copyOfRange(byteArr, mid, len), currentFrequency, depth+1);
+	}
+	//DAC APPROACH
+	
+	private ArrayList<ByteArrayWrapper> sanitizeFrequencies(HashMap<ByteArrayWrapper, Integer> frequencyTable){
+		// FILTERING NON REPEATING PATTERNS
+		for(ByteArrayWrapper invalidKeys : frequencyTable.keySet().stream().filter(new Predicate<ByteArrayWrapper>() {
+			@Override
+			public boolean test(ByteArrayWrapper t) {
+				return frequencyTable.get(t)==1;
+			}
+		}).toList()) {
+			frequencyTable.remove(invalidKeys);
+		}
+		
+		// CALCULATING MAXIMUM BYTES USED
+		int len = frequencyTable.size();
+		if(len>255) {
+			if(len>Math.pow(2, 16)) {
+				if(len>Math.pow(2, 24)) {
+					this.maxBytesUsed = 4;
+				} else {
+					this.maxBytesUsed = 3;
+				}
+			} else {
+				this.maxBytesUsed = 2;
+			}
+		} else {
+			this.maxBytesUsed = 1;
+		}
+		this.dictionaryLimit = ((int)Math.pow(255, this.maxBytesUsed));
+		
+		// FILTERING PATTERNS SMALLER THAN DICTIONARY LIMIT
+		for(ByteArrayWrapper invalidKeys : frequencyTable.keySet().stream().filter(new Predicate<ByteArrayWrapper>() {
+			@Override
+			public boolean test(ByteArrayWrapper t) {
+				return t.getData().length<=(maxBytesUsed+additionalMarkerBytes);
+			}
+		}).toList()) {
+			frequencyTable.remove(invalidKeys);
+		}
+		
+		// FILTERING OVERLAPPING PATTERNS
+		ArrayList<ByteArrayWrapper> lengthPrioritizedList = generateSortedBytesFromFrequency(frequencyTable, PRIORITY.FREQUENCY);
+		Set<ByteArrayWrapper> invalidKeys = new HashSet<ByteArrayWrapper>();
+		len = lengthPrioritizedList.size();
+		for(int i=0;i<len;i++) {
+			byte[] superset = lengthPrioritizedList.get(i).getData();
+			int supersetLen = superset.length;
+			for(int j=0;j<len;j++) {
+				byte[] subset = lengthPrioritizedList.get(j).getData();
+				int subsetLen = subset.length;
+				// CHECKING IF SUBSET
+				for(int k=0;k+subsetLen<supersetLen;k++) {
+					if(Arrays.equals(subset, Arrays.copyOfRange(superset, k, k+subsetLen))) {
+						invalidKeys.add(lengthPrioritizedList.get(j));
+						break;
+					}
+				}
+			}
+		}
+		for(ByteArrayWrapper key : invalidKeys) {
+			lengthPrioritizedList.remove(key);
+		}
+		return new ArrayList<>(lengthPrioritizedList.subList(0, Math.min(dictionaryLimit, lengthPrioritizedList.size())));
+	}
+	
+	enum PRIORITY {
+		FREQUENCY,
+		LENGTH
+	}
+	
+	private ArrayList<ByteArrayWrapper> generateSortedBytesFromFrequency(HashMap<ByteArrayWrapper, Integer> frequencyTable, PRIORITY priority) {
 		ArrayList<ByteArrayWrapper> sortedBytes = new ArrayList<>();
 		sortedBytes.addAll(frequencyTable.keySet());
-		sortedBytes.sort(new Comparator<ByteArrayWrapper>() {
-			@Override
-			public int compare(ByteArrayWrapper o1, ByteArrayWrapper o2) {
-				if(o2.getData().length!=o1.getData().length) return o2.getData().length-o1.getData().length;
-				return frequencyTable.get(o2).intValue()-frequencyTable.get(o1).intValue();
-			}
-		});
-		return new ArrayList<>(sortedBytes.subList(0, Math.min(this.dictionaryLimit, sortedBytes.size())));	//LIMIT ENTRIES;
+		switch (priority) {
+		case FREQUENCY:
+			sortedBytes.sort(new Comparator<ByteArrayWrapper>() {
+				@Override
+				public int compare(ByteArrayWrapper o1, ByteArrayWrapper o2) {
+					if(o2.getData().length!=o1.getData().length) return o2.getData().length-o1.getData().length;
+					return frequencyTable.get(o2).intValue()-frequencyTable.get(o1).intValue();
+				}
+			});
+			break;
+		case LENGTH:
+			sortedBytes.sort(new Comparator<ByteArrayWrapper>() {
+				@Override
+				public int compare(ByteArrayWrapper o1, ByteArrayWrapper o2) {
+					return frequencyTable.get(o2).intValue()-frequencyTable.get(o1).intValue();
+				}
+			});
+		}
+		return sortedBytes;
 	}
 
 }
