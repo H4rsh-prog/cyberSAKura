bab0e06935e6748c093e356af766e24988781019
Harsh <h4rshshrivastava@gmail.com>
2026-09-16 12:50:03 +0530

    added indice serialization

--- MODULES/LZ77/src/main/java/com/tools/module/service/SerializationService.java
+++ MODULES/LZ77/src/main/java/com/tools/module/service/SerializationService.java
@@ -38,4 +38,33 @@ public class SerializationService {
 		}
 		return dictionary;
 	}
+	public byte[] serializeIndices(ArrayList<Integer> indices) {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		int delta = 0;
+		try {
+			for(Integer n : indices) {
+				byte[] bytes = VarIntegerBytesParser.encode(n-delta);
+				baos.write(bytes);
+				delta = n;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return baos.toByteArray();
+	}
+	public ArrayList<Integer> deserializeIndices(byte[] bytes) {
+		ArrayList<Integer> indices = new ArrayList<Integer>();
+		int i = 0;
+		int delta = 0;
+		int size = bytes.length;
+		for(int j=i;j<size;j++) {
+			if((bytes[j] & 0x80) == 0) {
+				byte[] buffer = Arrays.copyOfRange(bytes, i, j+1);
+				delta += VarIntegerBytesParser.decode(bytes);
+				indices.add(delta);
+				i = j+1;
+			}
+		}
+		return indices;
+	}
 }
